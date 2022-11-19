d63452672285db78549e7c675bfc558b7c656533
Chris Niemela <christopher.niemela@ontariotechu.net>
2022-11-19 17:52:03 -0500

    updated db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = 
 {
-    "URI":"mongodb+srv://chris:LVXXcTXao0BpXEc@mongodbserver.opeu9gp.mongodb.net/musicLib"
+    "URI":"mongodb+srv://chris:ni227Hg6dAXiNAp@mongodbserver.opeu9gp.mongodb.net/musicLib"
 }
