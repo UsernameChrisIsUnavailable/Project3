8b3d2474a579f7c6a666676fc3796597d3c0207b
Chris Niemela <christopher.niemela@ontariotechu.net>
2022-11-19 19:05:48 -0500

    various changes

--- server/routes/music.js
+++ server/routes/music.js
@@ -19,10 +19,45 @@ router.get('/', (req,res,next)=>{
         else
         {
             res.render('music',{
-                title:'music list', 
+                title:'music', 
                 Musiclist: musiclist
             })
         }
     })
 })
+
+/*add operation*/
+/*get route for displaying the add-page -- create operation*/
+router.get('/add', (req,res,next)=>{
+
+});
+/*post route for processing the add page -- part of create operation*/
+router.post('/add', (req,res,next)=>{
+
+});
+
+
+
+/*edit operation*/
+/*get route for displaying the edit operation -- update operation*/
+router.get('/edit', (req,res,next)=>{
+
+});
+/*post route for displaying the edit operation -- part of create operation*/
+router.post('/edit', (req,res,next)=>{
+
+});
+
+
+/*delete operation*/
+/*get to perform delete operation -- deletion*/
+router.get('/add', (req,res,next)=>{
+
+});
+
+
+
+
+
+
 module.exports=router;
