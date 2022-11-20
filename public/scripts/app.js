430195efffc161938ab9ef10f7ab9289f9021b2f
Chris Niemela <christopher.niemela@ontariotechu.net>
2022-11-20 16:30:20 -0500

    nov 20

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,4 +5,16 @@
         console.log("App started");
     }
     window.addEventListener("load", Start);
+
+    let deleteButtons = document.querySelectorAll('.btn-danger');
+    for(button of deleteButtons)
+    {
+        button.addEventListener('click',(event)=>{
+            if(!confirm("Are you Sure?"))
+            {
+                event.prevent.Default();
+                window.location.assign('/musiclist');
+            }
+        })
+    }
 })();
--- server/routes/index.js
+++ server/routes/index.js
@@ -9,22 +9,5 @@ router.get('/', function(req, res, next) {
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home'});
 });
-/* GET home page. */
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About'});
-});
-/* GET home page. */
-router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects'});
-});
-/* GET home page. */
-router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact'});
-});
-/* GET music page. 
-router.get('/musiclist', function(req, res, next) {
-  res.render('index', { title: 'Musiclist'});
-});
-*/
 
 module.exports = router;
